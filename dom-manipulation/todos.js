8ab5fd27f9be59ebbfb4f0e59e925c18fc37627e
Sagar <dev.sagartmg@gmail.com>
2024-02-04 16:20:38 +0545

    domManipulation-todos

--- dom-manipulation/todos.js
+++ dom-manipulation/todos.js
@@ -5,25 +5,30 @@ let totalSum = 0;
 console.log(totalSum);
 
 let todos = [];
-// let todos = ["react","epxress"];
+// let todos = ["react","express"];
 
 function addTodo(event) {
   event.preventDefault();
   let input = document.getElementById("input-title").value;
-  todos.push(input)
-
-  let outputList = "";
-
-  todos.forEach((input,index) => {
-    outputList += `<li>${input} <button onclick="deleteTodo(${index})">delete</button> </li>`;
-  });
-
-  console.log(outputList)
-
-  document.getElementById("todos-list").innerHTML = outputList;
+  todos.push(input);
+  convertTodosToList()
 }
 
-function deleteTodo(index){
-    console.log(index);
+function deleteTodo(deletableIndex) {
+  todos = todos.filter((el, index) => index != deletableIndex);
+  convertTodosToList()
+}
 
+function convertTodosToList(){
+  let outputList = "";
+
+  todos.forEach((input, index) => {
+    outputList += `
+    <li>${input} <button onclick="deleteTodo(${index})">delete</button> </li>
+    `;
+  });
+
+  console.log(outputList); // "<li>react</li> <li>expres</li> "
+
+  document.getElementById("todos-list").innerHTML = outputList;
 }
---
+++ requestMethods.md
@@ -0,0 +1,20 @@
+
+CRUD operations in database
+    - Create
+    - Read
+    - Update
+    - DELETE
+
+
+Request Methods
+  - GET  
+  - POST  - create
+  - PUT  - update
+  - DELETE 
+
+
+STATUS CODES
+    - 2  SUCCESS
+    - 3  REDIRECT
+    - 4  CLIENT SIDE ERROR - FRONTEND
+    - 5  bACKEDN ERROR
